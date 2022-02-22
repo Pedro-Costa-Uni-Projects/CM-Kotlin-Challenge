5b4c6756e8665ebfaefb75242a445064639e9af4
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-02-22 14:57:12 +0000

    ?

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -3,10 +3,10 @@ package pt.ulusofona.cm.kotlin.challenge.models
 class Carro(identificador : String, motor : Motor) : Veiculo(identificador){
     override var cavalos = motor.cavalos
     override var cilindrada = motor.cilindrada
-    override var ligado = motor.ligado
+    override var ligado = false
 
     override fun requerCarta(): Boolean {
-        return false
+        return true
     }
 
     override fun toString(): String {
