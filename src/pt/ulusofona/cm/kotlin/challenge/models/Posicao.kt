8591e960e5130a483ca63eb301a64ca58be6bb3e
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-02-22 13:55:06 +0000

    Data class Posicao

--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -2,7 +2,7 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 
-class Posicao(var x: Int = 0, var y: Int = 0) {
+data class Posicao(var x: Int = 0, var y: Int = 0) {
 
     fun alterarPosicaoPara(x: Int, y: Int) {
         if ((this.x == x) && (this.y == y)) {
