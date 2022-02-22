9d28c81a4236e40a655d0bd3cb2064d47c800126
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-02-22 14:54:16 +0000

    ?

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -4,10 +4,10 @@ import pt.ulusofona.cm.kotlin.challenge.models.*
 import java.time.LocalDateTime
 
 fun main() {
-    /*Escrever aqui o codigo
+    /*Escrever aqui o codigo   */
     var pessoa = Pessoa("Pedro", java.sql.Timestamp.valueOf(LocalDateTime.now()))
     pessoa.carta = Carta()
     pessoa.comprarVeiculo(Carro("id-123", Motor(20,20)))
-    pessoa.moverVeiculoPara("id-123", 20, 20)
-    */
+    print(pessoa.pesquisarVeiculo("id-123").toString())
+
 }
