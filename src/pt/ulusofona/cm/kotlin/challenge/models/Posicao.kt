df79227ebcb4b9d693745120928abdbbb5ec7446
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-02-22 11:26:38 +0000

    Try with Integer

--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -2,14 +2,14 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 
-class Posicao(var x: Int = 0, var y: Int = 0) {
+class Posicao(var x: Integer = Integer(0), var y: Integer = Integer(0)) {
 
     fun alterarPosicaoPara(x: Int, y: Int) {
         if ((this.x.equals(x)) && (this.y.equals(y))) {
             throw AlterarPosicaoException()
         } else {
-            this.x = x
-            this.y = y
+            this.x = Integer(x)
+            this.y = Integer(y)
         }
     }
 
