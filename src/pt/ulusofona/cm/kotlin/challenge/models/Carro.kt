795116c378764a0ffbda8e7cb8f2b8243de5f631
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-02-22 14:31:58 +0000

    test

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -1,16 +1,14 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
 class Carro(identificador : String, motor : Motor) : Veiculo(identificador) {
-    var identificador2 = identificador
-    var motor = motor
-
+ 
     override fun requerCarta(): Boolean {
         return true
     }
 
     override fun toString(): String {
         val convertString = DateFormattor9000().formattor(this.dataDeAquisicao)
-        return "Carro | $identificador2 | $convertString | ${this.posicao}"
+        return "Carro | $identificador | $convertString | ${this.posicao}"
     }
 
 }
